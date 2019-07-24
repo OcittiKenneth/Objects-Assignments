5d2a15983263d88d4795d71c0696dc145438cecc
Strewik <52960396+Strewik@users.noreply.github.com>
2019-07-24 11:35:08 +0300

    Add files via upload

---
+++ kawooyaSteven.js
@@ -0,0 +1,61 @@
+//object house
+var house = {
+
+    type: "bangalow",
+    colour: "white",
+    location: "muyenga"
+
+} 
+document.write(house.type);
+
+
+//object shoe
+var shoe = {
+
+    brand: "addidas",
+    material: "leather",
+    purpose: "running"
+
+}
+document.write(shoe.brand);
+
+//object dog
+var dog = {
+
+    breed: "bloodhound",
+    origin: "Belgium",
+    top speed: "45mph"
+
+}
+document.write(dog.breed);
+
+//object gun
+var gun = {
+
+    name: "Barrett M82",
+    purpose: "snipper",
+    calliber: 50,
+
+    use: function () {
+
+        var user = "this gun is mainly used by snippers for long range shots";
+        return user;
+    }
+}
+document.write(gun.use());
+
+
+//object
+var politician = {
+
+    name: "that guy",
+    position: "MP",
+    work: "none",
+
+    use: function () {
+
+        var needHelp = "this guy is properly useless";
+        return user;
+    }
+}
+document.write(politician.needHelp());
