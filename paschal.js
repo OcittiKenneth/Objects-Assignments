685ebaab1c103653fdd3cda7a18ea92cd77438b9
sabbiti isaac paschal <51475517+paschalsabiti@users.noreply.github.com>
2019-07-24 08:53:49 +0300

    edition3

---
+++ paschal.js
@@ -0,0 +1,140 @@
+//==========================================================================================================================================
+//Sabbiti Isaac Paschal's work
+//=============================================================================================================================================
+//1.
+
+
+// declaring the city and defining it with keys and its values 
+var kampala={
+  // this contains 5 keys
+  // using the "camel notation to name."
+  size : "quiteBig",
+  hills : 5,
+  roads :"bad😆",
+  // 
+  weather :function(){
+    var climate = "not-the -best";
+    return climate;
+  },
+  // using a comma at the end of this object because it is still just a key.
+  // this key "vegetation" has one method
+  vegetation : function(){
+    var trees = "The trees are cut for firewood🤔";
+    return trees;
+  }
+}
+// this is a command calling to print out the contents of the key "hills" in the console
+console.log(kampala.hills);
+
+
+
+//============================================================================================================================================
+//2.
+
+
+// declaring the object city and defining it with keys and its values 
+var Entebbe={
+  // this contains 5 keys
+  // using the "camel notation to name."
+  size : "quiteSmall▫",
+  hills : 2,
+  roads :"superb🎁",
+  // 
+  weather :function(){
+    var climate = "great";
+    return climate;
+  },
+  // using a comma at the end of this object because it is still just a key.
+  // this key "vegetation" has one method
+  vegetation : function(){
+    var trees = "The trees are well preserved";
+    return trees;
+  }
+}
+// this is a command calling to print out the contents of the key "size" in the console
+console.log(Entebbe.size);
+
+
+//==========================================================================================================================================
+//3.
+
+
+// declaring the city and defining it with keys and its values 
+var Mukono={
+  // this contains 5 keys
+  // using the "camel notation to name."
+  size : "small_ish",
+  hills : 3,
+  roads :"poor😢",
+  // 
+  weather :function(){
+    var climate = "promising";
+    return climate;
+  },
+  // using a comma at the end of this object because it is still just a key.
+  // this key "vegetation" has one method
+  vegetation : function(){
+    var trees = "many in number";
+    return trees;
+  }
+}
+// this is a command calling to print out the contents of the key "weather",which is a function, in the console
+console.log(Mukono.weather());
+
+
+//=======================================================================================================================================================
+//4.
+
+
+// declaring the city and defining it with keys and its values 
+var Jinja={
+  // this contains 5 keys
+  // using the "camel notation to name."
+  size : "behemoth",
+  hills : 8,
+  roads :"getting better💯",
+  // 
+  weather :function(){
+    var climate = "hot and cold❤";
+    return climate;
+  },
+  // using a comma at the end of this object because it is still just a key.
+  // this key "vegetation" has one method
+  vegetation : function(){
+    var trees = "The city council cuts down trees rampantly but NGO's are planting more and more🥴";
+    return trees;
+  }
+}
+// this is a command calling to print out the contents of the key "vegetation" in the console
+console.log(Jinja.vegetation());
+
+//=================================================================================================================================================
+//5.
+
+
+// declaring the city and defining it with keys and its values 
+var mbarara={
+  // this contains 5 keys
+  // using the "camel notation to name."
+  size : "quiteBig😎",
+  hills : 20,
+  roads :"average👀",
+  // 
+  weather :function(){
+    var climate = "tropical👌";
+    return climate;
+  },
+  // using a comma at the end of this object because it is still just a key.
+  // this key "vegetation" has one method
+  vegetation : function(){
+    var trees = "loads of trees🌴";
+    return trees;
+  }
+}
+// this is a command calling to print out the contents of the key "roads" in the console
+console.log(mbarara.roads);
+
+
+//============================================================================================================================================================
+//Sabbiti Isaac Paschal's work 
+//===============================================================================================================================================
