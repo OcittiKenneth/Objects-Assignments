1bfbcbbaf16fb822794598561d624f2fa6888a42
kyla-abigail <52960543+kyla-abigail@users.noreply.github.com>
2019-07-24 11:44:26 +0300

    My git assignment

---
+++ kyla/objects assignment.js
@@ -0,0 +1,201 @@
+var cow = {
+    size:"big",
+    age:9,
+    gender:"male",
+    type:"persian",
+    product:"milk",
+    endurance:function() {
+        var health = "this cow is very healthy.";
+        return health;
+    },
+    quality:function(){
+        var harsh = "This cow can withstand harsh conditions.";
+        return harsh;
+    },
+}
+document.write(cow.endurance());
+document.write(cow.size);
+document.write("</br>");
+document.write(cow.age);
+document.write("</br>");
+document.write(cow.gender);
+document.write("</br>");
+document.write(cow.type);
+document.write("</br>");
+document.write(cow.product);
+document.write("</br>");
+
+document.write(cow.quality());
+document.write(cow.size);
+document.write("</br>");
+document.write(cow.age);
+document.write("</br>");
+document.write(cow.gender);
+document.write("</br>");
+document.write(cow.type);
+document.write("</br>");
+document.write(cow.product);
+document.write("</br>");
+
+
+var person ={
+    name:"john doe",
+    ages:120,
+    genders:"Male",
+    sizes:"medium",
+    timespan:"3 more years",
+    length:function(){
+        var healthy = "This person is not very healthy";
+        return healthy;
+    },
+    happiness:function(){
+        var family = "This person has had a long time to grow a family";
+        return family;
+    },
+};
+
+document.write(person.length());
+document.write(person.name);
+document.write("</br>");
+document.write(person.ages);
+document.write("</br>");
+document.write(person.genders);
+document.write("</br>");
+document.write(person.sizes);
+document.write("</br>");
+document.write(person.timespan);
+document.write("</br>");
+
+document.write(person.happiness());
+document.write(person.name);
+document.write("</br>");
+document.write(person.ages);
+document.write("</br>");
+document.write(person.genders);
+document.write("</br>");
+document.write(person.sizes);
+document.write("</br>");
+document.write(person.timespan);
+document.write("</br>");
+
+
+var invite ={
+    name:"anna",
+    ages:120,
+    genders:"female",
+    sizes:"medium",
+    plusone:"only one",
+    party:function(){
+        var party = "This person is invited to the party";
+        return party;
+    },
+    end:function(){
+        var end = "the party ends at midnight.";
+        return end;
+    },
+};
+
+document.write(invite.party());
+document.write(invite.name);
+document.write("</br>");
+document.write(invite.ages);
+document.write("</br>");
+document.write(invite.genders);
+document.write("</br>");
+document.write(invite.sizes);
+document.write("</br>");
+document.write(invite.plusone);
+document.write("</br>");
+
+document.write(invite.end());
+document.write(invite.name);
+document.write("</br>");
+document.write(invite.ages);
+document.write("</br>");
+document.write(invite.genders);
+document.write("</br>");
+document.write(invite.sizes);
+document.write("</br>");
+document.write(invite.plusone);
+document.write("</br>");
+
+var event ={
+    name:"anna",
+    age:120,
+    genders:"female",
+    dresscode:"black",
+    theme:"one last night",
+    festival:function(){
+        var gathering= "This is a themed event";
+        return gathering;
+    },
+    time:function(){
+        var venue = "the party ends at 2pm.";
+        return venue;
+    },
+};
+
+document.write(event.festival());
+document.write(event.name);
+document.write("</br>");
+document.write(event.ages);
+document.write("</br>");
+document.write(event.genders);
+document.write("</br>");
+document.write(event.dresscode);
+document.write("</br>");
+document.write(event.theme);
+document.write("</br>");
+
+document.write(event.time());
+document.write(event.name);
+document.write("</br>");
+document.write(event.ages);
+document.write("</br>");
+document.write(event.genders);
+document.write("</br>");
+document.write(event.dresscode);
+document.write("</br>");
+document.write(event.theme);
+document.write("</br>");
+
+var property ={
+    owner:"anna",
+    age:120,
+    gender:"female",
+    location:"kampala",
+    broker:"fred", 
+    home:function(){
+        var apartment= "This is your apartment";
+        return apartment;
+    },
+    friends:function(){
+        var place= "look to locate closer to your friends";
+        return place;
+    },
+};
+
+document.write(property.home());
+document.write(property.owner);
+document.write("</br>");
+document.write(property.age);
+document.write("</br>");
+document.write(property.gender);
+document.write("</br>");
+document.write(property.location);
+document.write("</br>");
+document.write(property.broker);
+document.write("</br>");
+
+document.write(property.friends());
+document.write(property.owner);
+document.write("</br>");
+document.write(property.age);
+document.write("</br>");
+document.write(property.gender);
+document.write("</br>");
+document.write(property.location);
+document.write("</br>");
+document.write(property.broker);
+document.write("</br>");
+
